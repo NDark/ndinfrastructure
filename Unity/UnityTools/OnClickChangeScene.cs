b6945ba983e9ce88e863721bcf8af142606dbb2e
ndarkpc <ndarkteng@gmail.com>
2017-04-15 11:26:17 +0800

    add OnClickChangeScene.cs

---
+++ Unity/UnityTools/OnClickChangeScene.cs
@@ -0,0 +1,17 @@
+﻿/**
+@file OnClickChangeScene.cs
+@author NDark
+@date 20170402 . file started.
+
+*/
+using UnityEngine;
+
+public class OnClickChangeScene : MonoBehaviour 
+{
+	public string m_SceneName = string.Empty ;
+
+	void OnClick()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ( m_SceneName );
+	}
+}
