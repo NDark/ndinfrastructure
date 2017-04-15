0392f6a9dddd1c976c22c77c2aa4a1f374faa87e
ndarkpc <ndarkteng@gmail.com>
2017-04-15 11:33:19 +0800

    add NGUIUtil/Lanugage/NGUISetLocalization.cs add NGUIUtil.cs

---
+++ Unity/NGUIUtil/Lanugage/NGUISetLocalization.cs
@@ -0,0 +1,20 @@
+﻿/**
+@file NGUISetLocalization.cs
+@author NDark
+@date 20170402 . file started.
+
+*/
+using UnityEngine;
+
+
+public class NGUISetLocalization : MonoBehaviour 
+{
+	public string m_LanguageKey = string.Empty ;
+	
+	public void SetLanguage()
+	{
+		NGUIInfrastructure.SetNGUILocalization( m_LanguageKey ) ;
+	}
+	
+
+}
---
+++ Unity/NGUIUtil/Lanugage/NGUIUtil.cs
@@ -0,0 +1,22 @@
+﻿/**
+@file NGUIInfrastructure.cs
+@author NDark
+@date 20170402 file started.
+
+*/
+#define NDU_INFRASTRUCTURE_USE_NGUI
+using UnityEngine;
+
+public static partial class NGUIInfrastructure
+{
+
+	public static void SetNGUILocalization( string _Language )
+	{
+		#if NDU_INFRASTRUCTURE_USE_NGUI		
+		Localization.language = _Language ;
+		#endif // NDU_INFRASTRUCTURE_USE_NGUI
+	}
+	
+	
+
+}
