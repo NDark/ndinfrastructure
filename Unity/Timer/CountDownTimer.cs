20ac21dd0d73476d43f6c4a51bcc0964e4ed9fb6
ndarkpc <ndarkteng@gmail.com>
2017-04-18 22:17:00 +0800

    add CountDownTimer.cs

---
+++ Unity/Timer/CountDownTimer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CountDownTimer
+{
+	public float IntervalSec { get{return m_IntervalSec;} set{ m_IntervalSec = value ;} } 
+
+	public void Rewind()
+	{
+		m_NextTime = Time.time + m_IntervalSec;
+	}
+
+	public bool IsReady() 
+	{
+		return ( Time.time > m_NextTime ) ;
+	}
+
+	float m_NextTime = 0.0f ;
+	float m_IntervalSec = float.MaxValue ;
+}
