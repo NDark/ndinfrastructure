48030fed1129bfa26e8ac2d012b8f8c2583cf2b2
ndarkpc <ndarkteng@gmail.com>
2017-08-19 18:11:24 +0800

    add EditorTools/EditorToolsPlayerPrefsDeleteAll.cs

---
+++ Unity/EditorTools/EditorToolsPlayerPrefsDeleteAll.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorToolsPlayerPrefsDeleteAll : MonoBehaviour 
+{
+	public void PlayerPrefsDeleteAll()
+	{
+		EditorTools.PlayerPrefsDeleteAll() ;
+	}
+
+
+}
