9bdab854a9c8f63cd905672c8bc8d86ed23fb981
ndark <ndarkteng@gmail.com>
2019-02-24 22:11:56 +0400

    add define guild ENABLE_NDINFRA_CUSTOM

--- Unity/ABFetcher/AssetBundleManager/AssetBundleManager.cs
+++ Unity/ABFetcher/AssetBundleManager/AssetBundleManager.cs
@@ -563,10 +563,13 @@ namespace AssetBundles
 						keysToRemove.Add(keyValue.Key);
 						continue;
 					}
-				
-					//Debug.Log("Downloading " + keyValue.Key + " is done at frame " + Time.frameCount);
-					string shortKey = RemovePostVariant(keyValue.Key) ;
 
+
+					//Debug.Log("Downloading " + keyValue.Key + " is done at frame " + Time.frameCount);
+					string shortKey = keyValue.Key ;
+#if ENABLE_NDINFRA_CUSTOM
+					shortKey = RemovePostVariant(keyValue.Key) ;
+#endif 
 					m_LoadedAssetBundles.Add(shortKey, 
 					new LoadedAssetBundle(download.assetBundle 
 #if ENABLE_NDINFRA_CUSTOM
@@ -647,11 +650,12 @@ namespace AssetBundles
 #endif // ENABLE_NDINFRA_DEBUG_INFO
 
 			AssetBundleLoadAssetOperation operation = null;
-	#if UNITY_EDITOR
+#if UNITY_EDITOR
 			if (SimulateAssetBundleInEditor)
 			{
+#if ENABLE_NDINFRA_CUSTOM			
 				assetBundleName = AddNAPostVariant( assetBundleName ) ;
-
+#endif 
 				string[] assetPaths = AssetDatabase.GetAssetPathsFromAssetBundleAndAssetName(assetBundleName, assetName);
 				if (assetPaths.Length == 0)
 				{
@@ -664,7 +668,7 @@ namespace AssetBundles
 				operation = new AssetBundleLoadAssetOperationSimulation (target);
 			}
 			else
-	#endif
+#endif
 			{
 				assetBundleName = RemapVariantName (assetBundleName);
 				LoadAssetBundle (assetBundleName);
