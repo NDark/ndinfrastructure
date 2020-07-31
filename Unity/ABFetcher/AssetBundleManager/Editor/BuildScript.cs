e461c9ddca120de0e8fbc0355b44bd4555faa666
ndark <ndarkteng@gmail.com>
2020-07-31 16:34:18 +0400

    upgrade Unity to 2017.4.40

--- Unity/ABFetcher/AssetBundleManager/Editor/BuildScript.cs
+++ Unity/ABFetcher/AssetBundleManager/Editor/BuildScript.cs
@@ -116,7 +116,7 @@ namespace AssetBundles
 				return "/test.exe";
 			case BuildTarget.StandaloneOSXIntel:
 			case BuildTarget.StandaloneOSXIntel64:
-			case BuildTarget.StandaloneOSXUniversal:
+			case BuildTarget.StandaloneOSX:
 				return "/test.app";
 #if !UNITY_2017_1_OR_NEWER
 			case BuildTarget.WebPlayer:
