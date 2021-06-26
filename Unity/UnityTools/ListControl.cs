b02943b44a9aa46a947feaf6aeea383a731cf289
ndarkteng <ndarkteng@gmail.com>
2021-06-26 18:23:02 +0800

    add ListControl.cs

---
+++ Unity/UnityTools/ListControl.cs
@@ -0,0 +1,51 @@
+﻿/**
+
+MIT License
+
+Copyright (c) 2017 - 2021 NDark
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+/**
+@file ListControl.cs
+@author NDark
+@date 20210626 . file started.
+
+*/
+using UnityEngine;
+using System.Collections.Generic;
+
+public static class ListControl
+{
+	public static void ShuffleList<T>(ref List<T> list)
+	{
+		int count = list.Count;
+		while (count > 0)
+		{
+			--count;
+			int index = Random.Range(0, count);
+			T value = list[index];
+			list[index] = list[count];
+			list[count] = value;
+		}
+	}
+
+}
+
