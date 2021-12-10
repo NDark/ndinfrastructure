98ea7e55d68a3d8a7804d22b88ac36f2ea42cbe3
ndarkteng <ndarkteng@gmail.com>
2021-12-10 11:18:48 +0800

    implement

---
+++ Unity/UnityProject/Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSDisplay : MonoBehaviour
+{
+	UnityEngine.UI.Text m_Text = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		m_Text = this.GetComponent<UnityEngine.UI.Text>();
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		++frame;
+		if (null != m_Text)
+		{
+			if (Time.realtimeSinceStartup - m_LastUpdate > 1.0f)
+			{
+				float average = frame / ( Time.realtimeSinceStartup - m_LastUpdate) ;
+				m_Text.text = string.Format("{0:0.00}", average);
+				frame = 0;
+				m_LastUpdate = Time.realtimeSinceStartup;
+			}
+			
+		}
+    }
+
+	float m_LastUpdate = 0.0f;
+	int frame= 0;
+}
---
+++ Unity/UnityProject/Assets/Scripts/TryCatchPerformanceManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TryCatchPerformanceManager : MonoBehaviour
+{
+	public bool m_IsLog = true;
+	public bool m_IsTrigger = false;
+	public bool m_IsDestroy = true;
+	public UnityEngine.UI.Text m_Text = null; 
+	public GameObject m_Prefab = null;
+
+	public void SwitchTrigger()
+	{
+		m_IsTrigger = !m_IsTrigger;
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Application.targetFrameRate = -1;
+		Debug.Log("Application.targetFrameRate"+ Application.targetFrameRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		TryTriggerTryCatch();
+    }
+
+	List<GameObject> objs = new List<GameObject>();
+
+	void TryTriggerTryCatch()
+	{
+
+
+		try
+		{
+			for (int i = 0; i < 100; ++i)
+			{
+				objs.Add(GameObject.Instantiate(m_Prefab));
+			}
+
+			if (m_IsTrigger)
+			{
+				m_Text.text = objs.Count.ToString();
+			}
+
+		}
+		catch (System.Exception e)
+		{
+			if (m_IsLog)
+			{
+				Debug.LogWarning(e.ToString());
+			}
+		}
+
+		if (m_IsDestroy)
+		{
+			foreach (var o in objs)
+			{
+				GameObject.Destroy(o);
+			}
+			objs.Clear();
+		}
+
+	}
+
+
+}
