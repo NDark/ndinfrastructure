b488ebfc5487630fea68ba33aa3756ec650b0272
ndarkteng <ndarkteng@gmail.com>
2025-08-30 18:30:12 +0800

    add m_MacOSStoreObj in SwitchOpenPlatform

--- Unity/UnityTools/SwitchOpenPlatform.cs
+++ Unity/UnityTools/SwitchOpenPlatform.cs
@@ -1,4 +1,36 @@
-﻿using System.Collections;
+﻿/**
+
+MIT License
+
+Copyright (c) 2017 - 2025 NDark
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+/**
+@file SwitchOpenPlatform.cs
+@author NDark
+@date 20231115. file started.
+@date 20250830. add m_MacOSStoreObj.
+
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,49 +40,75 @@ public class SwitchOpenPlatform : MonoBehaviour
 	public GameObject m_AppStore = null;
 	public GameObject m_WindowsStore = null;
 	public GameObject m_WebGLStore = null;
+    public GameObject m_MacOSStoreObj = null;
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Awake()
 	{
-		if (Application.platform == RuntimePlatform.IPhonePlayer)
-		{
-			if (null != m_AppStore) { m_AppStore.SetActive(true); }
-			if (null != m_GooglePlayMarket) { m_GooglePlayMarket.SetActive(false); }
-			if (null != m_WindowsStore) { m_WindowsStore.SetActive(false); }
-			if (null != m_WebGLStore) { m_WebGLStore.SetActive(false); }
-		}
-		else if (Application.platform == RuntimePlatform.Android)
-		{
-			if (null != m_AppStore) { m_AppStore.SetActive(false); }
-			if (null != m_GooglePlayMarket) { m_GooglePlayMarket.SetActive(true); }
-			if (null != m_WindowsStore) { m_WindowsStore.SetActive(false); }
-			if (null != m_WebGLStore) { m_WebGLStore.SetActive(false); }
-		}
-		else if (Application.platform == RuntimePlatform.WindowsPlayer)
-		{
-			if (null != m_AppStore) { m_AppStore.SetActive(false); }
-			if (null != m_GooglePlayMarket) { m_GooglePlayMarket.SetActive(false); }
-			if (null != m_WindowsStore) { m_WindowsStore.SetActive(true); }
-			if (null != m_WebGLStore) { m_WebGLStore.SetActive(false); }
-		}
-		else if (Application.platform == RuntimePlatform.WebGLPlayer)
-		{
-			if (null != m_AppStore) { m_AppStore.SetActive(false); }
-			if (null != m_GooglePlayMarket) { m_GooglePlayMarket.SetActive(false); }
-			if (null != m_WindowsStore) { m_WindowsStore.SetActive(false); }
-			if (null != m_WebGLStore) { m_WebGLStore.SetActive(true); }
-		}
-		else
-		{
-			if (null != m_AppStore) { m_AppStore.SetActive(false); }
-			if (null != m_GooglePlayMarket) { m_GooglePlayMarket.SetActive(true); }
-			if (null != m_WindowsStore) { m_WindowsStore.SetActive(false); }
-			if (null != m_WebGLStore) { m_WebGLStore.SetActive(false); }
-		}
+		SetupStructure();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Start()
+    {
+        SwitchObject();
+    }
+
+	void SwitchObject()
+	{
+        int validCount = 0;
+        foreach (var platform in m_SwitchingObjects)
+        {
+            bool isShow = (platform.Key == Application.platform);
+            if (isShow)
+            {
+                ++validCount;
+            }
+            platform.Value?.SetActive(isShow);
+        }
+
+
+#if UNITY_EDITOR
+        if ( Application.isEditor
+            && validCount <= 0
+            && m_SwitchingObjects.ContainsKey(RuntimePlatform.Android) 
+            )
+        {
+            Debug.LogWarning("SwitchOpenPlatform::SwitchObject() Application.isEditor");
+            m_SwitchingObjects[RuntimePlatform.Android]?.SetActive(true);
+        }
+#endif 
+
+    }
+
+	void SetupStructure()
+	{
+        m_SwitchingObjects.Clear();
+        if (null != m_AppStore)
+        {
+            m_SwitchingObjects.Add( RuntimePlatform.IPhonePlayer, m_AppStore);
+        }
+
+        if (null != m_GooglePlayMarket)
+        {
+            m_SwitchingObjects.Add( RuntimePlatform.Android , m_GooglePlayMarket);
+        }
+
+        if ( null != m_WindowsStore )
+        {
+            m_SwitchingObjects.Add( RuntimePlatform.WindowsPlayer , m_WindowsStore);
+        }
+
+        if ( null != m_WebGLStore )
+        {
+            m_SwitchingObjects.Add( RuntimePlatform.WebGLPlayer , m_WebGLStore);
+        }
+
+        if (null != m_MacOSStoreObj)
+        {
+            m_SwitchingObjects.Add(RuntimePlatform.OSXPlayer, m_MacOSStoreObj);
+        }
+
+    }
+
+    Dictionary<UnityEngine.RuntimePlatform, GameObject> m_SwitchingObjects = new Dictionary<RuntimePlatform, GameObject>();
 }
